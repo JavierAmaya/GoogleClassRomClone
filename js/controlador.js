07a0179f32cd441de4c043e4206ff7482775cc96
JavierAmaya <photojavhn@gmail.com>
2021-06-29 23:59:48 -0600

    views: clase, asignaciones

--- js/controlador.js
+++ js/controlador.js
@@ -1,1 +1,10 @@
 console.log('DOM cargado');
+
+const viewClase = () => {
+    console.log('entro a cambiar url');
+    //location.href('/views/clase.html');
+   // history.pushState(null, "", "/views/clase.html");
+    //location.reload();
+    location.href = "/views/clase.html";
+
+}
